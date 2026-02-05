87792cf7d11967ecd2d99f98b11fb539144fd49c
Castie7 <karlojimleano@gmail.com>
2026-02-05 15:52:57 +0800

    Added Https for security

--- backend/app/Config/Cookie.php
+++ backend/app/Config/Cookie.php
@@ -11,8 +11,6 @@ class Cookie extends BaseConfig
      * --------------------------------------------------------------------------
      * Cookie Prefix
      * --------------------------------------------------------------------------
-     *
-     * Set a cookie name prefix if you need to avoid collisions.
      */
     public string $prefix = '';
 
@@ -20,12 +18,6 @@ class Cookie extends BaseConfig
      * --------------------------------------------------------------------------
      * Cookie Expires Timestamp
      * --------------------------------------------------------------------------
-     *
-     * Default expires timestamp for cookies. Setting this to `0` will mean the
-     * cookie will not have the `Expires` attribute and will behave as a session
-     * cookie.
-     *
-     * @var DateTimeInterface|int|string
      */
     public $expires = 0;
 
@@ -33,8 +25,6 @@ class Cookie extends BaseConfig
      * --------------------------------------------------------------------------
      * Cookie Path
      * --------------------------------------------------------------------------
-     *
-     * Typically will be a forward slash.
      */
     public string $path = '/';
 
@@ -42,8 +32,6 @@ class Cookie extends BaseConfig
      * --------------------------------------------------------------------------
      * Cookie Domain
      * --------------------------------------------------------------------------
-     *
-     * Set to `.your-domain.com` for site-wide cookies.
      */
     public string $domain = '';
 
@@ -51,17 +39,14 @@ class Cookie extends BaseConfig
      * --------------------------------------------------------------------------
      * Cookie Secure
      * --------------------------------------------------------------------------
-     *
-     * Cookie will only be set if a secure HTTPS connection exists.
+     * 🔒 UPDATED: Set to true to enforce HTTPS
      */
-    public bool $secure = false;
+    public bool $secure = true;
 
     /**
      * --------------------------------------------------------------------------
      * Cookie HTTPOnly
      * --------------------------------------------------------------------------
-     *
-     * Cookie will only be accessible via HTTP(S) (no JavaScript).
      */
     public bool $httponly = true;
 
@@ -69,23 +54,6 @@ class Cookie extends BaseConfig
      * --------------------------------------------------------------------------
      * Cookie SameSite
      * --------------------------------------------------------------------------
-     *
-     * Configure cookie SameSite setting. Allowed values are:
-     * - None
-     * - Lax
-     * - Strict
-     * - ''
-     *
-     * Alternatively, you can use the constant names:
-     * - `Cookie::SAMESITE_NONE`
-     * - `Cookie::SAMESITE_LAX`
-     * - `Cookie::SAMESITE_STRICT`
-     *
-     * Defaults to `Lax` for compatibility with modern browsers. Setting `''`
-     * (empty string) means default SameSite attribute set by browsers (`Lax`)
-     * will be set on cookies. If set to `None`, `$secure` must also be set.
-     *
-     * @var ''|'Lax'|'None'|'Strict'
      */
     public string $samesite = 'Lax';
 
@@ -93,15 +61,6 @@ class Cookie extends BaseConfig
      * --------------------------------------------------------------------------
      * Cookie Raw
      * --------------------------------------------------------------------------
-     *
-     * This flag allows setting a "raw" cookie, i.e., its name and value are
-     * not URL encoded using `rawurlencode()`.
-     *
-     * If this is set to `true`, cookie names should be compliant of RFC 2616's
-     * list of allowed characters.
-     *
-     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#attributes
-     * @see https://tools.ietf.org/html/rfc2616#section-2.2
      */
     public bool $raw = false;
 }
